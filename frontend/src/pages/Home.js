46090a4b6aa3632c65a3664752bb2c217e04806b
SaloniSS <sxs170015@utdallas.edu>
2020-09-12 15:39:44 -0500

    set static user status

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -8,6 +8,17 @@ import "../styles/home.css";
 
 const Home = () => {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  let [saved, setSaved] = useState(false);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     if (isAuthenticated) {
+  //       const response = await getUser(user.sub);
+  //       if (response) setSaved(true);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []); // Or [] if effect doesn't need props or state
 
   const saveToDatabase = (isTeacher) => {
     db.collection("user")
@@ -38,13 +49,26 @@ const Home = () => {
 
   const connectToDatabase = async (isTeacher) => {
     const response = await getUser(user.sub);
+    console.log(response);
     if (!response) {
       await saveToDatabase(isTeacher);
       console.log("added to database with id", user.sub);
     } else {
       console.log("user already exists");
     }
+    setSaved(true);
   };
+
+  const checkForOldUser = async () => {
+    const response = await getUser(user.sub);
+    console.log(response);
+    console.log(saved);
+    if (response && !saved) setSaved(true);
+  };
+
+  if (isAuthenticated && !saved) {
+    checkForOldUser();
+  }
 
   const logoutWithRedirect = () =>
     logout({
@@ -68,25 +92,30 @@ const Home = () => {
       )}
       {isAuthenticated && (
         <>
-          <h3>Select your status:</h3>
-          <div style={{ padding: 10 }}>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => connectToDatabase(true)}
-              style={{ margin: 10 }}
-            >
-              Teacher
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => connectToDatabase(false)}
-              style={{ margin: 10 }}
-            >
-              Student
-            </Button>
-          </div>
+          {!saved && (
+            <>
+              <h3>Select your status:</h3>
+              <div style={{ padding: 10 }}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => connectToDatabase(true)}
+                  style={{ margin: 10 }}
+                >
+                  Teacher
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => connectToDatabase(false)}
+                  style={{ margin: 10 }}
+                >
+                  Student
+                </Button>
+              </div>
+            </>
+          )}
+
           <Link to={`/class/${user.sub.split("|")[1]}`}>
             Your Personal Class
           </Link>
