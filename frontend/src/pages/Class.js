aea44828fa127fb72a7426097f1de6306f598ea5
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-09-12 11:29:24 -0500

    add in material ui

--- frontend/src/pages/Class.js
+++ frontend/src/pages/Class.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
-import socketIOClient from "socket.io-client";
+import Grid from "@material-ui/core/Grid";
+import io from "socket.io-client";
 import Board from "./Board";
 
 const ENDPOINT = "http://localhost:4000/";
 
 const Class = (props) => {
   console.log(props.match.params.id); // compare this ID to local token to see if teacher
-  const socket = socketIOClient(ENDPOINT);
+  const socket = io.connect(ENDPOINT, { query: "id=userID" });
 
   return (
-    <>
-      <Board id="1" socket={socket}></Board>
-      <Board id="2" socket={socket}></Board>
-    </>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={9}>
+        <Board id="1" socket={socket}></Board>
+      </Grid>
+      <Grid item xs={12} sm={9}>
+        <Board id="2" socket={socket}></Board>
+      </Grid>
+    </Grid>
   );
 };
 
