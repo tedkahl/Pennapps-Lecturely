37df15bed5343e350ff23fcc106d5e571a2efb13
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-09-12 11:29:47 -0500

    add more socket logic

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,34 @@ const port = process.env.PORT || 4000;
 var teachers = io.of("/admin");
 var students = io.of("/student");
 
+const students = [];
+
 students.on("connection", (socket) => {
-  console.log("student connection");
+  // when student joins inform teacher
+  students.push({ socketID: socket.id, userID: socket.handshake.query["id"] });
+  teachers.emit("update_students");
+
   socket.on("drawing", (data) => {
     teachers.emit("drawing", data);
+  });
+
+  socket.on("disconnect", () => {
+    students.splice(
+      students.findIndex((item) => item.socketID === socket.id),
+      1
+    );
+    teachers.emit("update_students");
   });
 });
 
 teachers.on("connection", (socket) => {
   console.log("teacher connection");
+
+  socket.on("update_students", (data) => {
+    // implementing this as state in the frontend may erase previous student whiteboards uh
+    teachers.emit("update_students", students);
+  });
+
   socket.on("drawing", (data) => {
     students.emit("drawing", data);
   });
