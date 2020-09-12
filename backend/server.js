e1f1f07784e9c96c930053429259c006b158466b
Ted Kahl <tedkahl@gmail.com>
2020-09-12 03:20:21 -0700

    groups, still untested

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,43 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-
 const router = express.Router();
+const firebase = require("firebase");
+const { option } = require("yargs");
+require("firebase/firestore");
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
 
 const port = process.env.PORT || 4000;
+const max_teacherid = 1000; //if user id is below 1000, user is a teacher
 
-const max_teacherid = 1000;
+/*
+user
+{
+userid:x,
+name:x,
+sessionid:string,
+socketid:x,   -combine with userid? Can just remember
+group:x
+}
+
+*/
+
+//change this if using database
+function isTeacher(userid) {
+  return db
+    .collection("users")
+    .where("userid", "==", userid)
+    .where("isteacher", "==", true)
+    .get()
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+}
 
 //does nothing really
 io.on("connection", (socket) => {
@@ -16,36 +47,112 @@ io.on("connection", (socket) => {
   });
 });
 
+//add new user to database
+function saveUser(data) {
+  let newuser = {
+    userid: data.userid,
+    name: data.name,
+    sessionid: data.sessionid,
+    isteacher: data.isteacher,
+  };
+  db.collection("users")
+    .doc(data.userid)
+    .set(newuser)
+    .then(function () {
+      console.log("Document successfully written!");
+    })
+    .catch(function (error) {
+      console.error("Error writing document: ", error);
+    });
+}
+
 /*new users submit a join request with session id. Join a room for that session id, distinguished
 by teacher or student*/
+//data format {userid:x name:x sessionid:string isteacher:x}
 io.on("connection", (socket) => {
   socket.on("join request", (data) => {
-    let room = "" + data.sessionid;
+    saveUser(data);
 
-    if (data.userid < max_teacherid) {
+    if (isTeacher(data.userid)) {
       socket.join(room + "-teacher");
-    } else socket.join(room);
+    } else {
+      socket.join(room);
+      socket.to(data.sessionid + "-teacher").emit("join request", data); //tells the teacher who the student is
+    }
 
-    console.log(socket.id, " joined ", sessionid);
+    console.log(socket.id, " joined ", data.sessionid);
   });
 });
 
 /*Send teacher draw data to students and student data to teachers. Draw data includes
 sessionid and userid in addition to draw information*/
+//data format {userid:x, sessionid:string, ...(drawing information)}
+
+//backend call here seems bad, not real time
 io.on("connection", (socket) => {
   socket.on("draw data", (data) => {
-    let room = "" + data.sessionid;
+    if (!isTeacher(data.userid));
 
-    if (data.userid < max_teacherid) {
-      io.to(room).emit("draw data", data);
-    } else io.to(room + "-teacher").emit("draw data", data);
+    room = findGroupRoom(socket) || data.sessionid;
+
+    console.log(room);
+
+    if (data.isteacher) {
+      io.to(sessionid).emit("draw data", data); //if user is teacher, send data to session
+    } else {
+      io.to(sessionid + "-teacher").emit("draw data", data); //if user is student, send data to teacher of session
+      if (room != data.sessionid) {
+        io.to(room).emit("draw data", data); //if student is in a group, send data to group
+      }
+    }
   });
 });
+
+// data format "enable groups", {userid:x sessionid:string groupsize:x}
+io.on("connection", (socket) => {
+  socket.on("enable groups", (data) => {
+    if (!isTeacher(data.userid)) return;
+
+    let students = io.sockets.clients(data.sessionid);
+    let groupnum;
+    //if groupsize=3, and sessionid=55, put students 0, 1 and 2 into group 55-0, 3,4,5 into group 55-1, etc
+    if (data.groupsize > 1 && groupsize <= students.length) {
+      students.forEach((student, index) => {
+        groupnum = Math.floor(index / groupsize);
+        student.join(data.sessionid + "-" + groupnum);
+        db.collection("users")
+          .doc("" + student.id)
+          .update({ group: groupnum });
+      });
+    }
+  });
+});
+
+//{userid, sessionid studentid groupnum}
+//change a student's group
+io.on("connection", (socket) => {
+  socket.on("move student", (data) => {
+    if (!isTeacher(data.userid)) return;
+    let student = io.sockets.clients(data.sessionid).find((student) => {
+      return student.id == data.studentid;
+    });
+    student.leave(findGroupRoom(student));
+    student.join(data.sessionid + "-" + data.groupnum);
+
+    db.collection("users").doc(data.studentid).update({ group: groupnum });
+  });
+});
+
+function findGroupRoom(socket) {
+  socket.room.find((room) => {
+    return /.+-.+/.test(room);
+  });
+}
 
 router.get("/", (req, res) => {
   res.send("server running");
 });
 
 http.listen(port, () => {
-  console.log(`listening on ${process.env.PORT}`);
+  console.log(`listening on ${port}`);
 });
